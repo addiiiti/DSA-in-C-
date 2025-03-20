5a0298dad84350d867a5f2556a430d7ce6258f7b
addiiiti <aditiawez@gmail.com>
2025-03-20 23:23:35 +0530

    added file:Two Sum.cpp

---
+++ Day-6/two_sum.cpp
@@ -0,0 +1,25 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+class Solution {
+  public:
+      vector<int> twoSum(vector<int>& nums, int target) 
+      {
+          map<int,int> mpp;
+          for(int i =0;i<nums.size();i++)
+          {   
+             int a= nums[i];
+             int more=target-a;
+             if(mpp.find(more)!=mpp.end())
+             {
+              return {mpp[more],i};
+             }
+             mpp[a]=i;
+          }
+          return {-1,-1};
+          
+      }
+  };
